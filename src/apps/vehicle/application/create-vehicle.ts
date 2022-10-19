6d428ba49e236d019e6da42e861a49d0427f6d8e
Gonzalo Caballero <gacaballero@miuandes.cl>
2022-10-19 18:39:22 -0300

    feat(CreateVehicles): upload to opensearch after create

--- src/apps/vehicle/application/create-vehicle.ts
+++ src/apps/vehicle/application/create-vehicle.ts
@@ -3,13 +3,20 @@ import { inject, injectable } from 'inversify';
 import { TYPES } from 'apps/core/container/injection-types';
 import { User } from 'apps/core/domain/user';
 import { Vehicle } from 'apps/vehicle/domain/vehicle';
+import { VehicleSearchRepository } from 'apps/vehicle/domain/vehicle-search-repository';
 import { CreateVehicleDto, VehicleDBRepository } from 'apps/vehicle/domain/vehicles-db-repository';
 
 @injectable()
 export class CreateVehicle {
-  constructor(@inject(TYPES.VehicleDBRepository) private readonly vehicleDBRepository: VehicleDBRepository) {}
+  constructor(
+    @inject(TYPES.VehicleDBRepository) private readonly vehicleDBRepository: VehicleDBRepository,
+    @inject(TYPES.VehicleSearchRepository) private readonly vehicleSearchRepository: VehicleSearchRepository
+  ) {}
 
   async execute(user: User, vehicleData: CreateVehicleDto): Promise<Omit<Vehicle, 'owner'>> {
-    return this.vehicleDBRepository.create(user, vehicleData);
+    const vehicle = await this.vehicleDBRepository.create(user, vehicleData);
+    await this.vehicleSearchRepository.addToIndex(vehicle);
+
+    return vehicle;
   }
 }
