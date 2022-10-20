6f9d39221681b17e54052baa61f099963afbd983
Gonzalo Caballero <gonzalo@Gonzalos-MacBook-Pro-2.local>
2022-10-20 12:41:47 -0300

    feat(opensearch): use URL as env var

--- config.ts
+++ config.ts
@@ -4,6 +4,7 @@ const config = {
   NODE_ENV: process.env.NODE_ENV as 'production' | 'development',
   PORT: (process.env.PORT || 4000) as number,
   DATABASE_URL: process.env.DATABASE_URL,
+  OPENSEARCH_URL: process.env.OPENSEARCH_URL,
   PASSWORD_SALT: process.env.PASSWORD_SALT || 'PASSWORD_SALT',
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || 'REFRESH_TOKEN_SECRET',
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || 'ACCESS_TOKEN_SECRET',
--- src/infrastructure/opensearch/index.ts
+++ src/infrastructure/opensearch/index.ts
@@ -1,7 +1,9 @@
 import { Client } from '@opensearch-project/opensearch';
 
+import config from '@/config';
+
 export const OpenSearchClient = new Client({
-  node: 'http://admin:admin@car-auction-open-search-node-1:9200',
+  node: config.OPENSEARCH_URL,
   ssl: {
     rejectUnauthorized: false,
   },
